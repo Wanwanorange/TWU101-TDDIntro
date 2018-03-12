ce8c86f0a6bf4be34abc27edf066d0c79f36f6ae
wanwanorange <wanchen.y95@gmail.com>
2018-03-12 14:10:37 -0500

    Created test for shouldIncreaseMyBalanceWhenIDepositMoney and wrote a method to pass this test

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,22 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private double balance;
+
+    public Account() {
+        balance = 0;
+    }
+
+    public Account(double n) {
+        balance = n;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double depositMoney(double i) {
+        balance += i;
+        return balance;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,23 +1,28 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
-import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
+//    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        Account account = new Account(100);
+        double accountValue = account.getBalance();
+        assertThat(account.depositMoney(50), is(accountValue + 50));
     }
 
+
     @Test
-    @Ignore  // Remove each @Ignore and implement test
+//    @Ignore  // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
+//    @Ignore  // Remove each @Ignore and implement test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
 
     }
