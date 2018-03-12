b56ac6a4484794c7b37e2c2eaad5b2b69526bbb2
wanwanorange <wanchen.y95@gmail.com>
2018-03-12 13:51:46 -0500

    Updated compute method to pass the test shouldReturnOneWhenNumberIsZero

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -4,6 +4,8 @@ public class Factorial {
     public Integer compute(int i) {
         if (i == 1 || i == 2){
             return i;
+        } if (i == 0) {
+            return 1;
         }
         return -1;
     }
